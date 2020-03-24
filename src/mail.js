d84bd215551d1dff7f0d74b6664335f0b09134c2
pugongyingh <pgyhh@sina.cn>
2020-03-24 15:42:22 +0800

    Update mail.js

--- src/mail.js
+++ src/mail.js
@@ -8,8 +8,7 @@ const mailUtils = {
 
     return `您好 ${details.email}\n
     贵公司是否有程序开发，网站，微信小程序设计开发需求，或网络推广方面需要一些技术支持。
-如果您有这方面的疑问或需求请联系我们：www.hr9.top
-或许我们能帮您解决这些问题。`;
+如果您有这方面的疑问或需求请联系我们：www.hr9.top`;
   },
   getMailBody2: content => {
     const details = {
@@ -18,10 +17,9 @@ const mailUtils = {
       email: content.email
     };
 
-    return `您好 ${details.email}\n
+    return `
     贵公司是否有程序开发，网站，微信小程序设计开发需求，或网络推广方面需要一些技术支持。
-如果您有这方面的疑问或需求请联系我们：https://pensive-brahmagupta-05c0ba.netlify.com/
-或许我们能帮您解决这些问题。`;
+如果您有这方面的疑问或需求请联系我们：www.hr9.top`;
   },
   getMailBody3: content => {
     const details = {
@@ -32,7 +30,7 @@ const mailUtils = {
 
     return `
     如您有程序开发，网站，微信小程序设计开发需求，或网络推广方面需要一些技术支持。
-请联系我们：https://pensive-brahmagupta-05c0ba.netlify.com`;
+请联系我们：www.hr9.top`;
   },
   getMailBody4: content => {
     const details = {
@@ -42,9 +40,8 @@ const mailUtils = {
     };
 
     return `您好 ${details.email}\n
-    贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
-如果您有这方面的疑问或需求请联系我们，联系方式和介绍请到：www.hr9.top
-我们或许能帮您解决这些问题。`;
+    你是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
+如果您有这方面的疑问或需求请联系我们，联系方式和介绍请到：www.hr9.top`;
   },
   getMailBody5: content => {
     const details = {
@@ -55,8 +52,7 @@ const mailUtils = {
 
     return  `您好 ${details.email}\n
     贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
-如果您有这方面的疑问或需求请联系我们，详细介绍：www.hr9.top
-我们或许能帮您解决这些问题。`;
+如果您有这方面的疑问或需求请联系我，简介：www.hr9.top`;
   },
     getMailSub1: content => {
     const details = {
