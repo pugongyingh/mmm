633085b17cb814165ba9c2e429d471bcc9b206fd
pugongyingh <pgyhh@sina.cn>
2020-03-29 09:38:03 +0800

    Update mail.js

--- src/mail.js
+++ src/mail.js
@@ -17,7 +17,7 @@ const mailUtils = {
       email: content.email
     };
 
-    return `
+    return `您好 ${details.name}\n
     贵公司是否有程序开发，网站，微信小程序设计开发需求，或网络推广方面需要一些技术支持。
 如果您有这方面的疑问或需求请联系我：www.hr9.top`;
   },
@@ -28,7 +28,7 @@ const mailUtils = {
       email: content.email
     };
 
-    return `
+    return `您好 ${details.name}\n
     如您有程序开发，网站，微信小程序设计开发需求，或网络推广方面需要一些技术支持。
 请联系我：www.hr9.top`;
   },
@@ -39,9 +39,9 @@ const mailUtils = {
       email: content.email
     };
 
-    return `您好 ${details.email}\n
+    return `您好 ${details.name}\n
     你是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
-如果您有这方面的疑问或需求请联系我，联系方式和介绍请到：www.hr9.top`;
+如果您有这方面的疑问或需求请联系我，联系方式和介绍请到：www.hr9.top  我随时为您远程工作`;
   },
   getMailBody5: content => {
     const details = {
@@ -50,9 +50,9 @@ const mailUtils = {
       email: content.email
     };
 
-    return  `您好 ${details.email}\n
+    return  `您好 ${details.name}\n
     贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
-如果您有这方面的疑问或需求请联系我，简介：www.hr9.top`;
+如果您有这方面的疑问或需求请联系我，简介：www.hr9.top  我可以随时在线为您工作`;
   },
     getMailSub1: content => {
     const details = {
